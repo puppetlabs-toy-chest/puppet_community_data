be1fdec7c549153cde47fb5e26d5e8d707ce016e
Hailee Kenney <hailee@puppetlabs.com>
2013-06-06 14:37:38 -0700

    Add helpful whitespace

--- spec/unit/puppet_community_data/application_spec.rb
+++ spec/unit/puppet_community_data/application_spec.rb
@@ -37,18 +37,22 @@ describe PuppetCommunityData::Application do
       it 'is an empty hash before runnning' do
         expect(subject.opts).to eq({})
       end
+
       it 'sets the :google_account key after running' do
         subject.run
         expect(subject.opts[:google_account]).to eq 'changethis@acme.com'
       end
+
       it 'sets the :google_password key after running' do
         subject.run
         expect(subject.opts[:google_password]).to eq 'mypassword'
       end
+
       it 'sets the :spreadsheet_key after running' do
         subject.run
         expect(subject.opts[:spreadsheet_key]).to eq '1234key'
       end
+
       it 'sets the :github_oauth_token after running' do
         subject.run
         expect(subject.opts[:github_oauth_token]).to eq '1234token'
@@ -60,6 +64,7 @@ describe PuppetCommunityData::Application do
         it "accepts --google-account" do
           subject.run
         end
+
         it "returns the string account identifier" do
           subject.run
           expect(subject.google_account).to eq("changethis@acme.com")
@@ -70,6 +75,7 @@ describe PuppetCommunityData::Application do
         it "accepts --google-password" do
           subject.run
         end
+
         it "returns the string password identifier" do
           subject.run
           expect(subject.google_password).to eq("mypassword")
@@ -80,6 +86,7 @@ describe PuppetCommunityData::Application do
         it "accepts --spreadsheet-key" do
           subject.run
         end
+
         it "returns the string account identifier" do
           subject.run
           expect(subject.spreadsheet_key).to eq("1234key")
@@ -90,6 +97,7 @@ describe PuppetCommunityData::Application do
         it "accepts --github-oauth-token" do
           subject.run
         end
+
         it "returns the string account identifier" do
           subject.run
           expect(subject.github_oauth_token).to eq("1234token")
@@ -101,6 +109,7 @@ describe PuppetCommunityData::Application do
       it 'is a Octokit::Client instance' do
         expect(subject.github_api).to be_a_kind_of Octokit::Client
       end
+
       it 'sets auto_traversal to true to get paginated results' do
         expect(subject.github_api.auto_traversal).to eq(true)
       end
@@ -117,21 +126,27 @@ describe PuppetCommunityData::Application do
         it 'returns a hash of closed pull requests' do
           expect(subject.closed_pull_requests(repo)).to be_a_kind_of Hash
         end
+
         it 'has integer keys for the pull request numbers' do
           expect(subject.closed_pull_requests(repo).keys[0]).to be_a_kind_of Integer
         end
+
         it 'has values which are arrays' do
           expect(subject.closed_pull_requests(repo).values[0]).to be_a_kind_of Array
         end
+
         it 'has two element arrays for values' do
           expect(subject.closed_pull_requests(repo).values[0].length).to eq(2)
         end
+
         it 'has arrays for values which have an integer for the first element' do
           expect(subject.closed_pull_requests(repo).values[0][0]).to be_a_kind_of Integer
         end
+
         it 'has arrays for values which have true or false for the second element' do
           expect(subject.closed_pull_requests(repo).values[0][1]).to be_a_kind_of TrueClass or FalseClass
         end
+
         it 'includes pull request 123' do
           expect(subject.closed_pull_requests(repo).keys).to include(123)
         end

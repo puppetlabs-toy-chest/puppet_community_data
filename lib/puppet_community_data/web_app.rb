579b131193fc1f69018351fb911899d38ad9c89e
Hailee Kenney <hailee@puppetlabs.com>
2013-06-20 13:14:28 -0700

    Add database connection in WebApp
    Add connection to database in the WebApp class so that operations
    may be performed on the database with the Heroku application.

--- lib/puppet_community_data/web_app.rb
+++ lib/puppet_community_data/web_app.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 
 module PuppetCommunityData
   class WebApp < Sinatra::Base
+
+    # Extend Sinatra with ActiveRecord database connections.
+    register Sinatra::ActiveRecordExtension
+
     get '/' do
       "Hello world! From #{self.class.inspect}"
     end

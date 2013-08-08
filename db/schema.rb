5be6f725c2d146f37fccb34bf22aee59a924447e
Hailee Kenney <hailee@puppetlabs.com>
2013-08-08 11:31:50 -0700

    Remove footnotes
    Prior to this commit, the graphs were being described using footnotes.
    This was meant to be a temporary solution, and now that a better option
    has been found, the footnotes are no longer needed.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130730220801) do
+ActiveRecord::Schema.define(version: 20130805163630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20130730220801) do
     t.datetime "time_opened"
     t.datetime "time_closed"
     t.boolean  "from_community"
+    t.boolean  "closed"
   end
 
 end
